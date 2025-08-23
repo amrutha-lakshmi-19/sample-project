fcfc5a09ed7d88507f583c72a472980975735b2a
Amrutha Lakshmi <tunuguntlaamruthalakshmi@gmail.com>
2025-08-23 11:04:30 +0530

    Initial commit

---
+++ sample.js
@@ -0,0 +1,17 @@
+// sample.js
+
+// Function to greet a user
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// Call the function
+greet("Sudha");
+
+// Add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(5, 7);
+console.log("The sum is: " + result);
